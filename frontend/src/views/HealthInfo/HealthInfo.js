c011644a9ef9730ab64ce750491cfa9fa1336461
Sam Hachem <samhachem99@gmail.com>
2022-12-08 20:52:28 -0600

    have basic bmi bmr card done

--- frontend/src/views/HealthInfo/HealthInfo.js
+++ frontend/src/views/HealthInfo/HealthInfo.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { Icon } from "react-icons-kit";
+// import { Icon } from "react-icons-kit";
 import { eyeOff } from "react-icons-kit/feather/eyeOff";
 import { eye } from "react-icons-kit/feather/eye";
 import HomeNav from "../../components/HomeNav";
@@ -20,7 +20,7 @@ const HealthInfo = () => {
         gender: "male"
     })
 
-    const [userHealthData, setHealthData] = useState({
+    const [userHealthData, setUserHealthData] = useState({
         bmi: 0.0, 
         bmr: 0.0,
         required_calories: 0.0,
@@ -28,21 +28,18 @@ const HealthInfo = () => {
         physical_factor: 1
     })
 
-    const processUserHealthInfo = () => {
-        var data = {
-            bmi: 0.0, 
-            bmr: 0.0,
-            required_calories: 0.0,
-            bmi_class: "Normal Weight",
-            physical_factor: 1
-        }
-        data.bmi = (userData.weight)/(userData.height**2)*703;
+    const processUserHealthInfo = (userPerosnalData) => {
+        // console.log(userPerosnalData)
+        var data = userHealthData
+        data.bmi = ((userPerosnalData.weight)/(userPerosnalData.height**2)*703).toFixed(2);
 
-        if(userData.gender == "male") {
-            data.bmr = 66 + (6.23*userData.weight) + (12.7*userData.height) - (6.8*userData.age);
+        if(userPerosnalData.gender == "male") {
+            data.bmr = 66 + (6.23*userPerosnalData.weight) + (12.7*userPerosnalData.height) - (6.8*userPerosnalData.age);
         } else {
-            data.bmr = 655 + (4.35*userData.weight) + (4.7*userData.height) - (4.7*userData.age);
+            data.bmr = 655 + (4.35*userPerosnalData.weight) + (4.7*userPerosnalData.height) - (4.7*userPerosnalData.age);
         }
+
+        data.bmr = data.bmr.toFixed(2)
 
         if(data.bmr < 18.5) {
             data.bmi_class = "UnderWeight";
@@ -54,8 +51,11 @@ const HealthInfo = () => {
             data.bmi_class = "Obese";
         }
 
-        data.required_calories = bmr*userHealthData.physical_factor;
-
+        data.required_calories = (data.bmr*data.physical_factor).toFixed(2);
+        // console.log(data)
+        // setUserHealthData(data);
+        return data
+        // console.log(userHealthData)
     }
 
     const getUser = async () => {
@@ -63,12 +63,14 @@ const HealthInfo = () => {
         const { data } = await axios.get(url, config);
         //console.log(data);
         setUserData(data.data);
-        processUserHealthInfo();
+        setUserHealthData(processUserHealthInfo(data.data));
+        console.log(userHealthData)
     }
   
     useEffect(() => {
-    //console.log("called")
+    // console.log("called")
     getUser();
+    console.log(userData)
     }, []);
 
     const [err, setErr] = useState("");
@@ -78,6 +80,7 @@ const HealthInfo = () => {
     const [icon, setIcon] = useState(eyeOff);
 
     const handleChange = ({ currentTarget: input }) => {{
+        // console.log("change")
         setUserData({...userData, [input.name]: input.value });
         //console.log(newUserData);
         //console.log(userData);
@@ -95,25 +98,6 @@ const HealthInfo = () => {
 
     const handleSubmit = async (err) => {
         err.preventDefault();
-
-        try {
-            //console.log("submit1");
-            const url = "http://localhost:4000/api/userinfo";
-            //console.log("submit2");
-            //console.log(config)
-            const { data: res } = await axios.put(url, userData, config);
-            //console.log("submit3");
-            //console.log(res.message);
-            setSuccess(true);
-            setErrBool(false);
-        } catch (err) {
-            //console.log(err);
-            if (err.response && err.response.status >= 400 && err.response.status <= 500) {
-                setErr(err.response.data.message);
-                setSuccess(false);
-                setErrBool(true);
-            }
-        }
     }
 
     const config = {
@@ -133,122 +117,21 @@ const HealthInfo = () => {
 
         <div className="auth-wrapper">
                 <div className="auth-inner">
-                    <form onSubmit={handleSubmit}>
-                        <h3>Health Information</h3>
-                
-                        <div className="mb-3">
-                            <label>First name</label>
-                            <input
-                                type="text"
-                                name="firstName"
-                                className="form-control"
-                                placeholder="First name"
-                                defaultValue={userData.firstName}
-                                onChange={handleChange}
-                                required
-                            />
-                        </div>
-                
-                        <div className="mb-3">
-                            <label>Last name</label>
-                            <input 
-                                type="text" 
-                                name="lastName"
-                                className="form-control" 
-                                placeholder="Last name" 
-                                defaultValue={userData.lastName}
-                                onChange={handleChange}
-                                required
-                            />
-                        </div>
-                
-                        <div className="mb-3">
-                            <label>Email address</label>
-                            <input
-                                type="email"
-                                name="email"
-                                className="form-control"
-                                placeholder="Enter email"
-                                defaultValue={userData.email}
-                                onChange={handleChange}
-                                required
-                            />
-                        </div>
-
-                        <label>Password</label>
-                        <div className="mb-3 input-group">
-                            <input
-                                type={type}
-                                name="password"
-                                className="form-control border-end-0"
-                                placeholder="Enter password"
-                                required
-                                onChange={handleChange}
-                            />
-                            <span onClick={handleToggle} className="input-group-text bg-white" >
-                                <span className="bg-transparent">
-                                    <Icon icon={icon} size={15}/>
-                                </span>
-                            </span>
-                        </div>
-
-                        <div className="row mb-3">
-                            <div className="form-group col-md-4">
-                                <label>Height (in)</label>
-                                <input
-                                    type="number"
-                                    name="height"
-                                    className="form-control"
-                                    placeholder="Height"
-                                    defaultValue={userData.height}
-                                    onChange={handleChange}
-                                    required
-                                />
-                            </div>
-                            <div className="form-group col-md-4">
-                                <label>Weight (lbs) </label>
-                                <input
-                                    type="number"
-                                    name="weight"
-                                    className="form-control"
-                                    placeholder="Weight"
-                                    defaultValue={userData.weight}
-                                    onChange={handleChange}
-                                    required
-                                />
-                            </div>
-                            <div className="form-group col-md-4">
-                                <label>Age</label>
-                                <input
-                                    type="number"
-                                    name="age"
-                                    className="form-control"
-                                    placeholder="Age"
-                                    defaultValue={userData.age}
-                                    onChange={handleChange}
-                                    required
-                                />
-                            </div>
-                        </div>
-
-                        <div className="form-check form-check-inline">
-                            <input className="form-check-input" type="radio" name="gender" id="inlineRadio1" value="male" checked={userData.gender=="male"} onChange={handleChange}/>
-                            <label className="form-check-label" htmlFor="inlineRadio1"> Male </label>
-                        </div>
-                        <div className="form-check form-check-inline mb-3">
-                            <input className="form-check-input" type="radio" name="gender" id="inlineRadio2" value="female" checked={userData.gender=="female"} onChange={handleChange}/>
-                            <label className="form-check-label" htmlFor="inlineRadio2"> Female </label>
-                        </div>
-
-                        {errBool && <div className="error_msg"> {err} </div>}
-                        {success && <div className="success_msg"> Successfully applied! </div>}
-                
-                        <div className="d-grid">
-                            <button type="submit" className="btn btn-primary">
-                                Apply
-                            </button>
-                        </div>
-                    </form>
+                    <h3>Health Information</h3>
+                    <h4>Name: {userData.firstName} {userData.lastName}</h4>
+                    <div className="health_fact_box">
+                            <ul>
+                                <li><label>Height: {userData.height}</label></li>
+                                <li><label>Weight: {userData.weight}</label></li>
+                                <li><label>Gender: {userData.gender}</label></li>
+                                <li><label>Age: {userData.age}</label></li>
+                            </ul>
+                            <ul>
+                                <li><label>BMI: {userHealthData.bmi}</label></li>
+                                <li><label>BMI Class: {userHealthData.bmi_class}</label></li>
+                                <li><label>BMR: {userHealthData.bmr}</label></li>
+                            </ul>
+                    </div>
                 </div>
             </div>
       </div>
