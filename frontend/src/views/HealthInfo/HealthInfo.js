bbf28f0dd556c11448eee00248ae7928a790275c
Sam Hachem <samhachem99@gmail.com>
2022-12-09 00:14:48 -0600

    health information cad is done

--- frontend/src/views/HealthInfo/HealthInfo.js
+++ frontend/src/views/HealthInfo/HealthInfo.js
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-// import { Icon } from "react-icons-kit";
 import { eyeOff } from "react-icons-kit/feather/eyeOff";
-import { eye } from "react-icons-kit/feather/eye";
 import HomeNav from "../../components/HomeNav";
 /* eslint-disable */
 
@@ -25,7 +23,7 @@ const HealthInfo = () => {
         bmr: 0.0,
         required_calories: 0.0,
         bmi_class: "Normal Weight",
-        physical_factor: 1
+        physical_factor: 1.2
     })
 
     const processUserHealthInfo = (userPerosnalData) => {
@@ -41,13 +39,14 @@ const HealthInfo = () => {
 
         data.bmr = data.bmr.toFixed(2)
 
-        if(data.bmr < 18.5) {
+        console.log(data.bmi)
+        if(data.bmi < 18.5) {
             data.bmi_class = "UnderWeight";
-        } else if(18.5 <= data.bmr < 25) {
+        } else if(18.5 <= data.bmi && data.bmi < 25) {
             data.bmi_class = "Normal Weight";
-        } else if(25 <= data.bmr < 30) {
+        } else if(25 <= data.bmi && data.bmi < 30) {
             data.bmi_class = "Overweight";
-        } else {
+        } else if (data.bmi >= 30) {
             data.bmi_class = "Obese";
         }
 
@@ -70,7 +69,6 @@ const HealthInfo = () => {
     useEffect(() => {
     // console.log("called")
     getUser();
-    console.log(userData)
     }, []);
 
     const [err, setErr] = useState("");
@@ -80,35 +78,26 @@ const HealthInfo = () => {
     const [icon, setIcon] = useState(eyeOff);
 
     const handleChange = ({ currentTarget: input }) => {{
-        // console.log("change")
-        setUserData({...userData, [input.name]: input.value });
-        //console.log(newUserData);
-        //console.log(userData);
-    }};
-
-    const handleToggle = () => {
-        if (type == "password") {
-            setIcon(eye);
-            setType("text");
-        } else {
-            setIcon(eyeOff);
-            setType("password");
+        console.log("change")
+        var factor = 1.0;
+        if(input.value == 1) {
+            factor = 1.2
+        } else if(input.value == 2) {
+            factor = 1.37
+        } else if(input.value == 3) {
+            factor = 1.55
+        } else if(input.value == 4) {
+            factor = 1.725
+        } else if(input.value == 5) {
+            factor = 1.9
         }
-    }
-
-    const handleSubmit = async (err) => {
-        err.preventDefault();
-    }
+        setUserHealthData({...userHealthData, required_calories: (userHealthData.bmr*factor).toFixed(2)});
+    }};
 
     const config = {
       headers: {
         "auth-token": token
       }
-    }
-
-    const handleLogout = () => {
-        localStorage.removeItem("token");
-        window.location.reload();
     }
     
     return (
@@ -120,18 +109,31 @@ const HealthInfo = () => {
                     <h3>Health Information</h3>
                     <h4>Name: {userData.firstName} {userData.lastName}</h4>
                     <div className="health_fact_box">
-                            <ul>
-                                <li><label>Height: {userData.height}</label></li>
-                                <li><label>Weight: {userData.weight}</label></li>
-                                <li><label>Gender: {userData.gender}</label></li>
-                                <li><label>Age: {userData.age}</label></li>
-                            </ul>
-                            <ul>
-                                <li><label>BMI: {userHealthData.bmi}</label></li>
-                                <li><label>BMI Class: {userHealthData.bmi_class}</label></li>
-                                <li><label>BMR: {userHealthData.bmr}</label></li>
-                            </ul>
+                        <ul>
+                            <li><label>Height: {userData.height}</label></li>
+                            <li><label>Weight: {userData.weight}</label></li>
+                            <li><label>Gender: {userData.gender}</label></li>
+                            <li><label>Age: {userData.age}</label></li>
+                        </ul>
+                        <ul>
+                            <li><label>BMI: {userHealthData.bmi}</label></li>
+                            <li className={userHealthData.bmi_class == "UnderWeight" ? "underweight" : (userHealthData.bmi_class == "Normal Weight" ? "normal_weight": (userHealthData.bmi_class == "Overweight" ? "overweight": (userHealthData.bmi_class == "Obese" ? "obese" : "")))}><label>BMI Class: {userHealthData.bmi_class}</label></li>
+                            <li><label>BMR: {userHealthData.bmr}</label></li>
+                        </ul>
                     </div>
+                    <form name="activity_form" className="activity_form">
+                        <h4>What is your Activity Level?</h4>
+                        <select name="subject" id="subject" onChange={handleChange}>
+                            <option value="1" >Sedentary</option>
+                            <option value="2" >Light Active</option>
+                            <option value="3" >Moderatetely Active</option>
+                            <option value="4">Very Active</option>
+                            <option value="5">Extra Active</option>
+                        </select>
+                        <label>Your Required Calorie intake is:</label>
+                        <p>{userHealthData.required_calories}</p>
+                    </form>
+                    
                 </div>
             </div>
       </div>
